5a117f00348eb23dfffc4e1d920271cd933eb8cb
MrAsh-007 <mrash2006@gmail.com>
2026-01-30 18:27:20 +0500

    Style rehta hai aur phir logic banani hai

---
+++ Semester Result/app.js
@@ -0,0 +1,253 @@
+
+
+
+
+// let studentsCalculated = [
+//     {
+//         name: "Ali",
+//         id: "SE-101",
+//         semesters: {
+//             semester1: {
+//                 programmingFundamentals: 78,
+//                 ict: 72,
+//                 calculus: 70,
+//                 discreteMath: 75,
+//                 computerBasics: 80,
+//             },
+//             semester2: {
+//                 oop: 82,
+//                 dataStructures: 76,
+//                 linearAlgebra: 68,
+//                 digitalLogicDesign: 74,
+//                 softwareEngineeringIntro: 79,
+//             },
+//             semester3: {
+//                 databaseSystems: 85,
+//                 operatingSystems: 77,
+//                 algorithms: 73,
+//                 webEngineering: 81,
+//                 probabilityStats: 70,
+//             },
+//             semester4: {
+//                 softwareArchitecture: 80,
+//                 computerNetworks: 75,
+//                 artificialIntelligence: 72,
+//                 hci: 78,
+//                 informationSecurity: 74,
+//             },
+//             semester5: {
+//                 softwareProjectManagement: 83,
+//                 sqa: 79,
+//                 mobileAppDevelopment: 86,
+//                 cloudComputing: 81,
+//                 finalYearProject: 90,
+//             },
+//         },
+
+//     },
+// ];
+
+
+
+
+
+
+
+
+let studentsCalculated = [
+    {
+        name: "Ali",
+        id: "SE-101",
+        semesters: {
+            semester1: {
+                programmingFundamentals: 78,
+                ict: 72,
+                calculus: 70,
+                discreteMath: 75,
+                computerBasics: 80,
+            },
+        },
+        semester1TotalMarks : 0,
+    },
+];
+
+for(let i = 0; i < studentsCalculated.length ; i++){
+    console.log(studentsCalculated[i].semesters.semester1)
+}
+
+
+
+
+
+
+
+
+//   {
+//     name: "Ahmed",
+//     id: "SE-102",
+//     semesters: {
+//       semester1: {
+//         programmingFundamentals: 65,
+//         ict: 68,
+//         calculus: 62,
+//         discreteMath: 66,
+//         computerBasics: 70,
+//       },
+//       semester2: {
+//         oop: 72,
+//         dataStructures: 69,
+//         linearAlgebra: 64,
+//         digitalLogicDesign: 67,
+//         softwareEngineeringIntro: 71,
+//       },
+//       semester3: {
+//         databaseSystems: 74,
+//         operatingSystems: 70,
+//         algorithms: 66,
+//         webEngineering: 73,
+//         probabilityStats: 68,
+//       },
+//       semester4: {
+//         softwareArchitecture: 72,
+//         computerNetworks: 69,
+//         artificialIntelligence: 67,
+//         hci: 71,
+//         informationSecurity: 70,
+//       },
+//       semester5: {
+//         softwareProjectManagement: 75,
+//         sqa: 72,
+//         mobileAppDevelopment: 78,
+//         cloudComputing: 74,
+//         finalYearProject: 82,
+//       },
+//     },
+//   },
+
+//   {
+//     name: "Usman",
+//     id: "SE-103",
+//     semesters: {
+//       semester1: {
+//         programmingFundamentals: 88,
+//         ict: 80,
+//         calculus: 78,
+//         discreteMath: 82,
+//         computerBasics: 85,
+//       },
+//       semester2: {
+//         oop: 86,
+//         dataStructures: 84,
+//         linearAlgebra: 76,
+//         digitalLogicDesign: 81,
+//         softwareEngineeringIntro: 83,
+//       },
+//       semester3: {
+//         databaseSystems: 88,
+//         operatingSystems: 82,
+//         algorithms: 80,
+//         webEngineering: 85,
+//         probabilityStats: 78,
+//       },
+//       semester4: {
+//         softwareArchitecture: 84,
+//         computerNetworks: 81,
+//         artificialIntelligence: 79,
+//         hci: 83,
+//         informationSecurity: 80,
+//       },
+//       semester5: {
+//         softwareProjectManagement: 87,
+//         sqa: 85,
+//         mobileAppDevelopment: 90,
+//         cloudComputing: 88,
+//         finalYearProject: 92,
+//       },
+//     },
+//   },
+
+//   {
+//     name: "Hamza",
+//     id: "SE-104",
+//     semesters: {
+//       semester1: {
+//         programmingFundamentals: 70,
+//         ict: 66,
+//         calculus: 64,
+//         discreteMath: 68,
+//         computerBasics: 72,
+//       },
+//       semester2: {
+//         oop: 74,
+//         dataStructures: 71,
+//         linearAlgebra: 67,
+//         digitalLogicDesign: 70,
+//         softwareEngineeringIntro: 73,
+//       },
+//       semester3: {
+//         databaseSystems: 76,
+//         operatingSystems: 72,
+//         algorithms: 70,
+//         webEngineering: 74,
+//         probabilityStats: 69,
+//       },
+//       semester4: {
+//         softwareArchitecture: 73,
+//         computerNetworks: 71,
+//         artificialIntelligence: 68,
+//         hci: 72,
+//         informationSecurity: 70,
+//       },
+//       semester5: {
+//         softwareProjectManagement: 76,
+//         sqa: 74,
+//         mobileAppDevelopment: 78,
+//         cloudComputing: 75,
+//         finalYearProject: 80,
+//       },
+//     },
+//   },
+
+//   {
+//     name: "Bilal",
+//     id: "SE-105",
+//     semesters: {
+//       semester1: {
+//         programmingFundamentals: 60,
+//         ict: 62,
+//         calculus: 58,
+//         discreteMath: 61,
+//         computerBasics: 64,
+//       },
+//       semester2: {
+//         oop: 65,
+//         dataStructures: 63,
+//         linearAlgebra: 60,
+//         digitalLogicDesign: 62,
+//         softwareEngineeringIntro: 66,
+//       },
+//       semester3: {
+//         databaseSystems: 68,
+//         operatingSystems: 65,
+//         algorithms: 63,
+//         webEngineering: 67,
+//         probabilityStats: 62,
+//       },
+//       semester4: {
+//         softwareArchitecture: 66,
+//         computerNetworks: 64,
+//         artificialIntelligence: 61,
+//         hci: 65,
+//         informationSecurity: 63,
+//       },
+//       semester5: {
+//         softwareProjectManagement: 69,
+//         sqa: 67,
+//         mobileAppDevelopment: 71,
+//         cloudComputing: 68,
+//         finalYearProject: 75,
+//       },
+//     },
+//   },
+// ];
+
