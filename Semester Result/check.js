2b3a9939e33f8abe3eb0e484d954fcf1475e889e
MrAsh-007 <mrash2006@gmail.com>
2026-02-07 20:46:48 +0500

    Work, Work, And only Work

--- Semester Result/check.js
+++ Semester Result/check.js
@@ -2,89 +2,220 @@ let studentsData = [
     {
         name: "Ali",
         id: "SE-101",
-        semesters: {
-            semester1ObtainedMarks: {
-                programmingFundamentals: 78,
-                ict: 72,
-                calculus: 70,
-                discreteMath: 75,
-                computerBasics: 80,
+        semesters: [
+            {
+                semesterObtainedMarks: {
+                    programmingFundamentals: 78,
+                    ict: 72,
+                    calculus: 70,
+                    discreteMath: 75,
+                    computerBasics: 80,
+                }
             },
-        },
-        semester1result: {
-            semester1TotalMarks : 0,
-            semester1Percentage : 0,
-            grade : ""
-        },
+            {
+                semesterObtainedMarks: {
+                    oop: 82,
+                    dataStructures: 76,
+                    linearAlgebra: 68,
+                    digitalLogicDesign: 74,
+                    softwareEngineeringIntro: 79,
+                }
+            },
+            {
+                semesterObtainedMarks: {
+                    databaseSystems: 85,
+                    operatingSystems: 77,
+                    algorithms: 73,
+                    webEngineering: 81,
+                    probabilityStats: 70,
+                }
+            },
+            {
+                semesterObtainedMarks: {
+                    softwareArchitecture: 80,
+                    computerNetworks: 75,
+                    artificialIntelligence: 72,
+                    hci: 78,
+                    informationSecurity: 74,
+                }
+            },
+            {
+                semesterObtainedMarks: {
+                    softwareProjectManagement: 83,
+                    sqa: 79,
+                    mobileAppDevelopment: 86,
+                    cloudComputing: 81,
+                    finalYearProject: 90,
+                }
+            }
+        ],
+        semesterresult: [
+            {
+                semesterName: "Semester 1",
+                semesterTotalMarks: 0,
+                semesterPercentage: 0,
+                grade: ""
+            },
+            {
+                semesterName: "Semester 2",
+                semesterTotalMarks: 0,
+                semesterPercentage: 0,
+                grade: ""
+            },
+            {
+                semesterName: "Semester 3",
+                semesterTotalMarks: 0,
+                semesterPercentage: 0,
+                grade: ""
+            },
+            {
+                semesterName: "Semester 4",
+                semesterTotalMarks: 0,
+                semesterPercentage: 0,
+                grade: ""
+            },
+            {
+                semesterName: "Semester 5",
+                semesterTotalMarks: 0,
+                semesterPercentage: 0,
+                grade: ""
+            },
+        ]
     },
 ];
 
+
+
+
+
+
+
+
+// let semestersDefaultData = {
+//     semester1 : {},
+// }
+
+let defaultvar1 = JSON.parse(JSON.stringify(studentsData[0].semesters[0].semesterObtainedMarks));
+// semestersDefaultData.semester1 = defaultvar1;
+
 let sumOfTotalMarks = 0;
 
-
-let totalMarks = 100;
-
-let semestersDefaultData = {
-    semester1 : {},
-}
-
-let defaultvar1 = JSON.parse(JSON.stringify(studentsData[0].semesters.semester1ObtainedMarks));
-semestersDefaultData.semester1 = defaultvar1;
-
-
-for(let key in defaultvar1){
+for (let key in defaultvar1) {
     defaultvar1[key] = 100;
     sumOfTotalMarks += defaultvar1[key];
 }
 
 
-let semester1var = studentsData[0].semesters.semester1ObtainedMarks;
 
-for(let key in semester1var){
-    studentsData[0].semester1result.semester1TotalMarks += semester1var[key];
+
+
+
+
+
+let semestervar = 0;
+
+function randomone() {
+    for (let i = 0; i < studentsData[0].semesters.length; i++) {
+
+        semestervar = studentsData[0].semesters[i].semesterObtainedMarks;
+
+        if (semestervar == studentsData[0].semesters[0].semesterObtainedMarks) {
+            for (let key in semestervar) {
+                studentsData[0].semesterresult[0].semesterTotalMarks += semestervar[key];
+            }
+        }
+        if (semestervar == studentsData[0].semesters[1].semesterObtainedMarks) {
+            for (let key in semestervar) {
+                studentsData[0].semesterresult[1].semesterTotalMarks += semestervar[key];
+            }
+        }
+        if (semestervar == studentsData[0].semesters[2].semesterObtainedMarks) {
+            for (let key in semestervar) {
+                studentsData[0].semesterresult[2].semesterTotalMarks += semestervar[key];
+            }
+        }
+        if (semestervar == studentsData[0].semesters[3].semesterObtainedMarks) {
+            for (let key in semestervar) {
+                studentsData[0].semesterresult[3].semesterTotalMarks += semestervar[key];
+            }
+        }
+        if (semestervar == studentsData[0].semesters[4].semesterObtainedMarks) {
+            for (let key in semestervar) {
+                studentsData[0].semesterresult[4].semesterTotalMarks += semestervar[key];
+            }
+        }
+    }
+}
+randomone()
+
+let totalMarks = 100;
+
+
+
+
+
+let semesterTotalMarksVar = [
+    studentsData[0].semesterresult[0].semesterTotalMarks,
+    studentsData[0].semesterresult[1].semesterTotalMarks,
+    studentsData[0].semesterresult[2].semesterTotalMarks,
+    studentsData[0].semesterresult[3].semesterTotalMarks,
+    studentsData[0].semesterresult[4].semesterTotalMarks,
+]
+
+for (let i = 0; i < studentsData[0].semesterresult.length; i++) {
+    studentsData[0].semesterresult[i].semesterPercentage = semesterTotalMarksVar[i] / sumOfTotalMarks * 100;
 }
 
-let semester1TotalMarksVar = studentsData[0].semester1result.semester1TotalMarks;
 
-studentsData[0].semester1result.semester1Percentage = semester1TotalMarksVar / sumOfTotalMarks * 100;
-
-
-let semester1PercentageVar = studentsData[0].semester1result.semester1Percentage;
+let semesterPercentageVar = [
+    studentsData[0].semesterresult[0].semesterPercentage,
+    studentsData[0].semesterresult[1].semesterPercentage,
+    studentsData[0].semesterresult[2].semesterPercentage,
+    studentsData[0].semesterresult[3].semesterPercentage,
+    studentsData[0].semesterresult[4].semesterPercentage,
+]
 
 console.log(studentsData);
-console.log(semestersDefaultData);
-console.log(semester1TotalMarksVar);
-console.log(semester1PercentageVar);
+console.log(semesterTotalMarksVar);
+console.log(semesterPercentageVar);
 
 
-function gradeMarker (){
-    if(semester1PercentageVar >= 90){
-        return studentsData[0].semester1result.grade = "A+";
-    }else if(semester1PercentageVar < 90 && semester1PercentageVar >= 80){
-        return studentsData[0].semester1result.grade = "A";
-    }else if(semester1PercentageVar < 80 && semester1PercentageVar >= 70){
-        return studentsData[0].semester1result.grade = "B";
-    }else if(semester1PercentageVar < 70 && semester1PercentageVar >= 60){
-        return studentsData[0].semester1result.grade = "C";
-    }else if(semester1PercentageVar < 60 && semester1PercentageVar >= 50){
-        return studentsData[0].semester1result.grade = "D";
-    }else if(semester1PercentageVar < 50 ){
-        return studentsData[0].semester1result.grade = "Fail";
+function gradeMarker() {
+  for (let i = 0; i < semesterPercentageVar.length; i++) {
+
+    if (semesterPercentageVar[i] >= 90) {
+      studentsData[0].semesterresult[i].grade = "A+";
+    } 
+    else if (semesterPercentageVar[i] >= 80) {
+      studentsData[0].semesterresult[i].grade = "A";
+    } 
+    else if (semesterPercentageVar[i] >= 70) {
+      studentsData[0].semesterresult[i].grade = "B";
+    } 
+    else if (semesterPercentageVar[i] >= 60) {
+      studentsData[0].semesterresult[i].grade = "C";
+    } 
+    else if (semesterPercentageVar[i] >= 50) {
+      studentsData[0].semesterresult[i].grade = "D";
+    } 
+    else {
+      studentsData[0].semesterresult[i].grade = "Fail";
     }
 
+  }
 }
+gradeMarker()
 
-gradeMarker ()
-
-
-
+for(let i = 0; i < studentsData[0].semesterresult.length; i++){
+    console.log(studentsData[0].semesterresult[i].grade)
+}
 
 
 let rightContainer = document.getElementById("rightContainer");
 
 const createSemesterReportCardHTMLThroughMap = () => {
-    const studentsDataMapReturn = studentsData.map((data) => {
-        return `<h3 class="semester-heading">Semester 1</h3>
+    let studentsDataMapReturn = studentsData.map((data) => {
+        return `<h3 class="semester-heading">${data.semesterresult.semesterName}</h3>
                 <div class="report-card">
                     <table>
                         <tr>
@@ -94,41 +225,41 @@ const createSemesterReportCardHTMLThroughMap = () => {
                         </tr>
                         <tr>
                             <th>English</th>
-                            <td>${data.semesters.semester1ObtainedMarks.calculus}</td>
+                            <td>${data.semesters[0].semesterObtainedMarks.calculus}</td>
                             <td>${totalMarks}</td>
                         </tr>
                         <tr>
                             <th>Urdu</th>
-                            <td>${data.semesters.semester1ObtainedMarks.computerBasics}</td>
+                            <td>${data.semesters[0].semesterObtainedMarks.computerBasics}</td>
                             <td>${totalMarks}</td>
                         </tr>
                         <tr>
                             <th>Math</th>
-                            <td>${data.semesters.semester1ObtainedMarks.discreteMath}</td>
+                            <td>${data.semesters[0].semesterObtainedMarks.discreteMath}</td>
                             <td>${totalMarks}</td>
                         </tr>
                         <tr>
                             <th>Islamiat</th>
-                            <td>${data.semesters.semester1ObtainedMarks.ict}</td>
+                            <td>${data.semesters[0].semesterObtainedMarks.ict}</td>
                             <td>${totalMarks}</td>
                         </tr>
                             <th>Computer</th>
-                            <td>${data.semesters.semester1ObtainedMarks.programmingFundamentals}</td>
+                            <td>${data.semesters[0].semesterObtainedMarks.programmingFundamentals}</td>
                             <td>${totalMarks}</td>
                         </tr>
                     </table>
                     <table>
                         <tr>
                             <th>Total Obtained Marks</th>
-                            <td>${data.semester1result.semester1TotalMarks} / ${sumOfTotalMarks}</td>
+                            <td>${data.semesterresult[0].semesterTotalMarks} / ${sumOfTotalMarks}</td>
                         </tr>
                         <tr>
                             <th>Percentage</th>
-                            <td>${data.semester1result.semester1Percentage}%</td>
+                            <td>${data.semesterresult[0].semesterPercentage}%</td>
                         </tr>
                         <tr>
                             <th>Grade</th>
-                            <td>${data.semester1result.grade}</td>
+                            <td>${data.semesterresult[0].grade}</td>
                         </tr>
                     </table>
                 </div>`
